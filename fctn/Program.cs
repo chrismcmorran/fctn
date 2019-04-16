beec7fed2de4168f73e340feb6fe838c45f92280
Christopher McMorran <christopher.mcmorran@uottawa.ca>
2019-04-16 17:52:42 -0400

    Created project.

---
+++ fctn/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fctn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckArguments(args);
+            
+        }
+
+        /**
+         * Determines if enough arguments have been provided to the program.
+         * If no arguments have been provided then the program will exit.
+         */
+        private static void CheckArguments(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                string usageMessage = "Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " /path/to/directory string_to_find";
+                Console.WriteLine(usageMessage);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
